0d7cb02c77a01d0dd0db21919afda8f29bc315bb
erikmjohnson <erikarisma98@gmail.com>
2019-06-19 18:08:30 -0700

    made some fixes

--- main/src/component/JobSearch/JobSearch.js
+++ main/src/component/JobSearch/JobSearch.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom';
+import {Link as RouterLink} from 'react-router-dom';
 import JobSearchForm from '../JobSearchForm/JobSearchForm';
 import * as JobSearchActions from '../../action/jobSearch-actions';
 import * as authAuctions from '../../action/auth-actions';
